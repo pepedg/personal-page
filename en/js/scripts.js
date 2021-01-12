5fe00dd30d10bd24f9644ffd74eba39d1613754d
José Díaz <73610677+Pp-wveDev@users.noreply.github.com>
2021-01-12 03:48:27 +0100

    Added resources to language folders

---
+++ en/js/scripts.js
@@ -0,0 +1,71 @@
+/*!
+    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#sideNav",
+    });
+
+
+
+
+})(jQuery); // End of use strict
+
+
+
+// Progress Bars
+var lang = {
+  "html": "100%",
+  "css": "90%",
+  "javascript": "70%",
+  "php": "55%",
+  "angular": "65%"
+};
+
+var multiply = 4;
+
+$.each( lang, function( language, pourcent) {
+
+  var delay = 700;
+  
+  setTimeout(function() {
+    $('#'+language+'-pourcent').html(pourcent);
+  },delay*multiply);
+  
+  multiply++;
+
+});
